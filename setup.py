43903630e4ee5dddebb177e6f37ba35ba55eb31d
Evan Young <DocCodes@users.noreply.github.com>
2017-08-04 20:31:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 from distutils.core import setup
 setup(
-   name = 'steam',
-   packages = ['steam'],
-   version = '1.2.0',
-   description = 'A Steam API',
+   name = 'wemo',
+   packages = ['wemo'],
+   version = '1.0.0',
+   description = 'A Wemo API',
    author = 'Evan Young',
-   url = 'https://github.com/DocCodes/steam',
-   download_url = 'https://github.com/DocCodes/steam/archive/master.tar.gz',
-   keywords = ['steam', 'api', 'userdata'],
+   url = 'https://github.com/DocCodes/wemo',
+   download_url = 'https://github.com/DocCodes/wemo/archive/master.tar.gz',
+   keywords = ['wemo', 'api', 'automation'],
    classifiers = [
       'Development Status :: 5 - Production',
       'Intended Audience :: Developers',
@@ -17,8 +17,7 @@ setup(
       'Programming Language :: Python :: 3.6'
    ],
    install_requires = [
-      'requests',
-      'beautifulsoup4'
+      'requests'
    ],
    python_requires = '~=3.6'
 )
