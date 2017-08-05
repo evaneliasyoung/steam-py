fc76e0a60578339c0020872116ea7f544d833544
Evan Young <evanyoung62@icloud.com>
2017-08-04 20:41:46 -0500

    Revert "Update setup.py"
    This reverts commit 43903630e4ee5dddebb177e6f37ba35ba55eb31d.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 from distutils.core import setup
 setup(
-   name = 'wemo',
-   packages = ['wemo'],
-   version = '1.0.0',
-   description = 'A Wemo API',
+   name = 'steam',
+   packages = ['steam'],
+   version = '1.2.0',
+   description = 'A Steam API',
    author = 'Evan Young',
-   url = 'https://github.com/DocCodes/wemo',
-   download_url = 'https://github.com/DocCodes/wemo/archive/master.tar.gz',
-   keywords = ['wemo', 'api', 'automation'],
+   url = 'https://github.com/DocCodes/steam',
+   download_url = 'https://github.com/DocCodes/steam/archive/master.tar.gz',
+   keywords = ['steam', 'api', 'userdata'],
    classifiers = [
       'Development Status :: 5 - Production',
       'Intended Audience :: Developers',
@@ -17,7 +17,8 @@ setup(
       'Programming Language :: Python :: 3.6'
    ],
    install_requires = [
-      'requests'
+      'requests',
+      'beautifulsoup4'
    ],
    python_requires = '~=3.6'
 )
